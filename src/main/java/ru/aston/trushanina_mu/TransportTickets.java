5aaacb027ed46a605c394235d4d1a65b6d82124f
MsHitai <mmalafeeva@yandex.ru>
2023-08-29 23:43:22 +0300

    Initial commit

---
+++ src/main/java/ru/aston/trushanina_mu/TransportTickets.java
@@ -0,0 +1,13 @@
+package ru.aston.trushanina_mu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportTickets {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportTickets.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aston/trushanina_mu/TransportTicketsTests.java
@@ -0,0 +1,13 @@
+package ru.aston.trushanina_mu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportTicketsTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
